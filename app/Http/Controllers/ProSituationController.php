27830092d3c7d8e397fa082a0a71688badc2e026
adamshan <adamualiyu199@gmail.com>
2020-05-11 13:56:43 +0100

    read-delete@ProSituation

--- app/Http/Controllers/ProSituationController.php
+++ app/Http/Controllers/ProSituationController.php
@@ -4,31 +4,41 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\ProSituation;
+use App\APIError;
 
 class ProSituationController extends Controller
 {
-    //
     public function find($id){
         $prosituation = ProSituation::find($id);
         if($prosituation == null) {
-            abort(404, "No group found with id $id");
+            $unauthorized = new APIError;
+            $unauthorized->setStatus("404");
+            $unauthorized->setCode("PRO_SITUATION_FIND");
+            $unauthorized->setMessage("pro_situation id not found");
+
+            return response()->json($unauthorized, 404); 
         }
         return response()->json($prosituation);
     }
 
-    public function get($limit=null , $s=null){
-       
-        $prosituations = ProSituation::where('name','LIKE','%'.$s.'%')->paginate($limit); 
-        return response()->json($prosituations);
+    public function get(Request $request){
+      $limit = $request->limit;
+      $s = $request->s; 
+      $page = $request->page; 
+      $prosituations = ProSituation::where('name','LIKE','%'.$s.'%')
+                                     ->paginate($limit); 
+      return response()->json($prosituations);
     }
-    
-   
-
 
     public function delete($id){
         $prosituation = ProSituation::find($id);
         if($prosituation == null) {
-            abort(404, "No group found with id $id");
+            $unauthorized = new APIError;
+            $unauthorized->setStatus("404");
+            $unauthorized->setCode("PRO_SITUATION_DELETE");
+            $unauthorized->setMessage("pro_situation id not found");
+
+            return response()->json($unauthorized, 404); 
         }
         $prosituation->delete($prosituation);
         return response(null);
