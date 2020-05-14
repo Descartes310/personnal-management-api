9711545e9a1a0993f084ac0936dbb1b5214204e0
Ben677 <meztsacar@gmail.com>
2020-05-14 11:39:13 +0100

    Merge BlogCat and BlogCategory controller

--- app/Http/Controllers/BlogCategoryController.php
+++ app/Http/Controllers/BlogCategoryController.php
@@ -9,11 +9,53 @@ use App\APIError;
 class BlogCategoryController extends Controller
 {
 
+    public function create(Request $request)
+    {
+        $request->validate([
+            'title' => 'required'
+        ]);
+
+        $data = $request->only([
+            'title',
+            'description'
+        ]);
+
+        $blogcat = BlogCategory::create($data);
+        return response()->json($blogcat);
+    }
+
+
+
+    public function update(Request $request, $id)
+    {
+        $blogcat = BlogCategory::find($id);
+        if ($blogcat == null) {
+            $apiError = new APIError;
+            $apiError->setStatus("404");
+            $apiError->setCode("BLOGCATEGORY_ID_NOT_EXISTING");
+            $apiError->setErrors(['id' => 'blog category id not existing']);
+
+            return response()->json($apiError, 404);
+        }
+
+        $request->validate([
+            'title' => 'required'
+        ]);
+
+        $data = $request->only([
+            'title',
+            'description'
+        ]);
+
+        $blogcat->update($data);
+        return response()->json($blogcat);
+    }
+
+
     /**
      * delete a blog Category
      * @author Brell Sanwouo
      */
-
     public function delete ($id){
         $blogCategory = BlogCategory::find($id);
         if(!$blogCategory){
