e30b273d1f5fc8151a832cfeb9fcd2a545eee4d4
adamshan <adamualiyu199@gmail.com>
2020-05-16 09:33:30 +0100

    update of license.php

--- app/License.php
+++ app/License.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class License extends Model
 {
+    public function license_types() {
+        return $this->belongsToMany('App\LicenseType');
+    }
     protected $guarded = [];
 }
