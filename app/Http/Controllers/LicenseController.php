f4549542115003969fc614ea906c538c438e35e3
Descartes Fowo <descartesouambo@gmail.com>
2020-05-18 10:36:41 +0100

    Validating start_date in license

--- app/Http/Controllers/LicenseController.php
+++ app/Http/Controllers/LicenseController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use App\License;
 use App\LicenseType;
 use App\User;
+use Carbon\Carbon;
 use App\Http\Controllers\Controller;
 
 
@@ -69,14 +70,15 @@ class LicenseController extends Controller{
 
 
      public function create (Request $request){
+        $now = Carbon::now(); 
         $request->validate([
            'user_id' => 'required',
            'license_type_id' => 'required',
-           'requested_start_date' => 'required|date',
+           'requested_start_date' => 'required|date|after:'.$now,
            'requested_days' => 'required|numeric|min:0',
            'is_active' => 'required|boolean',
            'status' => 'in:PENDING,APPROVED,REJECTED,CANCELLED'
-    ]);
+        ]);
      
         $data = $request->only([
           'user_id',
@@ -90,7 +92,7 @@ class LicenseController extends Controller{
           'file',
           'is_active',
           'status'
-    ]);
+        ]);
          if(User::find($request->user_id) == null){
             $apiError = new APIError;
             $apiError->setStatus("400");
@@ -143,9 +145,8 @@ class LicenseController extends Controller{
                 $file->move($destinationPath, $safeName);
                 $path = "$relativeDestination/$safeName";
             }
+            $data['file'] = $path;
         }
-
-         $data['file'] = $path;
 
         $license = License::create($data);
         return response()->json($license);
